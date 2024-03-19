2afa2ab151de358d7ea4059f334f935efa74d417
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2024-03-19 21:00:25 +0200

    +

--- app.js
+++ app.js
@@ -5707,3 +5707,32 @@ let list   = document.querySelector('ul');
 //Передача коллбэка для работы с DOM на JavaScript
 
 
+// const array1 = ['a', 'b', 'c'];
+
+// array1.forEach((element) => console.log(element));
+// function forEach(selector, callback){
+//     let elements = document.querySelectorAll(selector)
+//     elements.forEach(callback)
+// }
+
+// forEach('.elem', function(elem) {
+//     elem.textContent += '!'
+// 	console.log(elem); // выведет по очереди найденные элементы
+// });
+
+
+//Передача порядкового номера в коллбэк на JavaScript
+
+// function forEach(selector, func) {
+// 	let elems = document.querySelectorAll(selector);
+	
+// 	for (let i = 0; i < elems.length; i++) {
+// 		func(elems[i], i);
+// 	}
+// }
+// forEach('.elem', function(elem, index) {
+// 	elem.textContent = index + elem.textContent;
+// });
+
+
+//Передача DOM элемента параметром функции
