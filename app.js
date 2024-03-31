05698e5a9ce206e829d91705791aea8e3c552023
Alex <98063450+Alex-Torzuk@users.noreply.github.com>
2024-03-31 22:02:17 +0300

    +

--- app.js
+++ app.js
@@ -5736,3 +5736,11 @@ let list   = document.querySelector('ul');
 
 
 //Передача DOM элемента параметром функции
+function setText(elem, text){
+	elem.textContent = text;
+}
+let elem1 = document.getElementById('elem1');
+setText(elem1, 'text1');
+
+let elem2 = document.getElementById('elem2');
+setText(elem2, 'text2');
